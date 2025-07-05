43c824c6f705a8d7695855ba02c00cd6af0637a7
Your Name <your_email@example.com>
2025-07-05 14:45:16 +0800

    header 显示标题

--- public/partials/canvas-header.php
+++ public/partials/canvas-header.php
@@ -1,5 +1,8 @@
 <div class="header_left">
-    顶栏
+    <div class="header_title">
+        <!-- 通过产品ID 显示产品名 -->
+        <?php echo get_the_title($product_id); ?>
+    </div>    
 </div>
 <div class="header_right">    
     <button id="forward"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-clockwise" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2z"/><path d="M8 4.466V.534a.25.25 0 0 1 .41-.192L10.77 2.308a.25.25 0 0 1 0 .384l-2.36 1.966A.25.25 0 0 1 8 4.466"/></svg></button>
