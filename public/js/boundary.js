6e6932966b4a54c40000af31988c5f51f8ad950a
Your Name <your_email@example.com>
2025-07-09 05:58:21 +0800

    修改边缘

--- public/js/boundary.js
+++ public/js/boundary.js
@@ -1,15 +1,15 @@
 // 绘制边缘限制
 const boundaryCanvas = document.getElementById('boundaryLayer');
 const boundaryCtx = boundaryCanvas.getContext('2d');
-const BOUNDARY_MARGIN = 50; // 边缘限制的宽度
+const BOUNDARY_MARGIN = 25; // 边缘限制的宽度
 function drawBoundary() {
     boundaryCtx.clearRect(0, 0, boundaryCanvas.width, boundaryCanvas.height);
     // 绘制半透明红色边框
     boundaryCtx.fillStyle = 'rgba(255, 0, 0, 1)';
     // 上边框
-    boundaryCtx.fillRect(0, 0, boundaryCanvas.width, BOUNDARY_MARGIN);
+    boundaryCtx.fillRect(0, 0, boundaryCanvas.width, BOUNDARY_MARGIN / 2);
     // 下边框
-    boundaryCtx.fillRect(0, boundaryCanvas.height - BOUNDARY_MARGIN, boundaryCanvas.width, BOUNDARY_MARGIN);
+    boundaryCtx.fillRect(0, boundaryCanvas.height - BOUNDARY_MARGIN /2 , boundaryCanvas.width, BOUNDARY_MARGIN /2);
     // 左边框
     boundaryCtx.fillRect(0, BOUNDARY_MARGIN, BOUNDARY_MARGIN, boundaryCanvas.height - 2 * BOUNDARY_MARGIN);
     // 右边框
