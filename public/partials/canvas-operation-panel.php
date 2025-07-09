5f681d2ba3c6d5af578d0c6fd0a85bdc7ee4caa1
Your Name <your_email@example.com>
2025-07-09 09:02:31 +0800

    修改文字和按钮

--- public/partials/canvas-operation-panel.php
+++ public/partials/canvas-operation-panel.php
@@ -760,12 +760,12 @@ $plugin_url = plugin_dir_url(__FILE__);
     <!-- 文字内容 (隐藏) -->
     <div id="content-wenzi" class="content-pane">
         <textarea id="customText" rows="4" style="width: 100%; margin-bottom: 10px; padding: 8px; border: 1px solid #ddd; border-radius: 4px;" placeholder="请输入要添加的文字..."></textarea>
-        <button id="addTextBtn" style="margin-top: 10px;" onclick="addText()">
+        <button id="addTextBtn" class ="btn-custom" style="margin-top: 10px;" onclick="addText()">
             <svg viewBox="0 0 24 24" width="16" height="16" style="vertical-align: middle;">
                 <path fill="currentColor"
                     d="M18.5,4L19.66,8.35L18.7,8.61C18.25,7.74 17.79,6.87 17.26,6.43C16.73,6 16.11,6 15.5,6H13V16.5C13,17 13,17.5 13.33,17.75C13.67,18 14.33,18 15,18V19H9V18C9.67,18 10.33,18 10.67,17.75C11,17.5 11,17 11,16.5V6H8.5C7.89,6 7.27,6 6.74,6.43C6.21,6.87 5.75,7.74 5.3,8.61L4.34,8.35L5.5,4H18.5Z" />
             </svg>
-            添加文字
+            Add Text
         </button>
         <hr>
         <div id="content-wenzi-control"></div>
