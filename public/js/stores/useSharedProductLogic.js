bbb6d5326974143290c6dc99d8b110ab2eb0648c
Your Name <your_email@example.com>
2025-08-27 23:03:33 +0800

    添加共享 pinia store

---
+++ public/js/stores/useSharedProductLogic.js
@@ -0,0 +1,9 @@
+// 多个页面 共享数据和算法
+const useSharedProductLogic = Pinia.defineStore('sharedProductLogic', () => {
+    
+
+    return {
+       
+    };
+});
+window.useSharedProductLogic = useSharedProductLogic;
